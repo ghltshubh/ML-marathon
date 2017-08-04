2ca56e9fd6886afd7f74973c5cb7c3d10466070f
ghltshubh <ghltshubh@gmail.com>
2017-08-04 16:04:24 -0500

    Day 12

--- Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
+++ Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
+X = dataset.iloc[:, [2,3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0, test_size = 0.25)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -44,11 +44,6 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
 
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
@@ -62,8 +57,4 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+
