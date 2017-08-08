4707ac2af0188a2afb7bf114f36a868ddf27e3f5
ghltshubh <ghltshubh@gmail.com>
2017-08-08 02:21:22 -0500

    Day 14

--- Part 3 - Classification/Section 20 - Random Forest Classification/classification_template.py
+++ Part 3 - Classification/Section 20 - Random Forest Classification/classification_template.py
@@ -1,4 +1,4 @@
-# Decision Tree Classifier
+# Classification template
 
 # Importing the libraries
 import numpy as np
@@ -7,25 +7,23 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2,3]].values
+X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
-# Spltting the dataset in to the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0, test_size = 0.25)
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# Feature scaling
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fittinf classifier to the Training set
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(X_train, y_train)
+# Fitting classifier to the Training set
+# Create your classifier here
 
-# Predictinf the Test set results
+# Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
@@ -38,30 +36,32 @@ X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(("red", "green")))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(("red", "green"))(i), label = j)
-plt.title("Decision Tree (Training set)")
-plt.xlabel("Age")
-plt.ylabel("Estimated Salary")
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
 plt.show()
 
-# Visualising the Testing set results
+# Visualising the Test set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(("red", "green")))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(("red", "green"))(i), label = j)
-plt.title("Decision Tree (Training set)")
-plt.xlabel("Age")
-plt.ylabel("Estimated Salary")
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
 plt.show()
--- Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.py
+++ Part 3 - Classification/Section 20 - Random Forest Classification/random_forest_classification.py
@@ -1,31 +1,32 @@
-# Decision Tree Classifier
+# Random Forrest Classifier
 
-# Importing the libraries
+# Importing th libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
+dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, [2,3]].values
 y = dataset.iloc[:, 4].values
 
-# Spltting the dataset in to the Training set and Test set
+# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0, test_size = 0.25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# Feature scaling
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fittinf classifier to the Training set
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+# Fitting Random Forest 
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=10, criterion="entropy", max_depth=None)
 classifier.fit(X_train, y_train)
 
-# Predictinf the Test set results
+
+# Predicting the Text set results
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
@@ -44,12 +45,13 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(("red", "green"))(i), label = j)
-plt.title("Decision Tree (Training set)")
+plt.title("Random Forest (Training set)")
 plt.xlabel("Age")
 plt.ylabel("Estimated Salary")
+plt.legend()
 plt.show()
 
-# Visualising the Testing set results
+# Visualising the Test set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -61,7 +63,11 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(("red", "green"))(i), label = j)
-plt.title("Decision Tree (Training set)")
+plt.title("Random Forest (Test set)")
 plt.xlabel("Age")
 plt.ylabel("Estimated Salary")
+plt.legend()
 plt.show()
+
+
+
